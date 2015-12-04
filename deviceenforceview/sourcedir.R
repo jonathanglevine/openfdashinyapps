1d2d73c24f0fb93edb264b17c9c07637f642fa44
jonathanglevine <jstat10@me.com>
2015-12-04 08:16:35 -0500

    Fixed sourcedir in reportview and device enforceview

--- deviceenforceview/sourcedir.R
+++ deviceenforceview/sourcedir.R
@@ -1,5 +1,8 @@
 SOURCEDIR <- '../sharedscripts/'
-# SOURCEDIR <- 'sharedscripts/'
+if (!dir.exists(SOURCEDIR))
+{
+  SOURCEDIR <- 'sharedscripts/'
+}
 source( paste0( SOURCEDIR, 'helpfunctions.r') )
 source( paste0( SOURCEDIR, 'serverhelpers.R') )
 source( paste0( SOURCEDIR, 'uihelpers.R') )
